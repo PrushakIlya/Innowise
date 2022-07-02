362917fdf839316da8dbd15ae198910f70cb3c44
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-03 01:08:10 +0300

    task-2

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 include_once 'src/Task2.php';
 $task_2 = new ('Prushak\Innowise\Task2');
 
-$date = '24.01.1000';
-$match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|0?[1-9]{1}).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $date);
-if (!is_string($date) || $match===0) {
+$date = 1;
+
+if (!is_string($date)) {
     throw new InvalidArgumentException('main function only accepts strings and correct format. Input was: '.$date);
 }
 echo $task_2->main($date);
--- src/Task2.php
+++ src/Task2.php
@@ -9,11 +9,19 @@ class Task2
 {
     public function main(string $date):string
     {
+        $this->check($date);
         $date = explode('.', $date);
         $time = date('d.m.y');
         $time = explode('.', $time);
         $year = 2023;
         $time[1]<=$date[1] && $year = 2022;
         return date_diff(new DateTime(), new DateTime($year.'-'.$date[1].'-'.$date[0]))->days;
+    }
+    private function check(string $date)
+    {
+        $match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|0?[1-9]{1}).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $date);
+        if ($match===0) {
+            throw new InvalidArgumentException('main function only accepts correct format. Input was: '.$date);
+        }
     }
 }
