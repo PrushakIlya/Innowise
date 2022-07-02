e015cdc331a5dc1d2ce2e741e84f77e72228d8fb
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-02 20:45:18 +0300

    task-1

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once 'src/Task.php';
-$task = new ('Prushak\Innowise\Task');
+include_once 'src/Task1.php';
+$task_1 = new ('Prushak\Innowise\Task1');
 
 $inputNumber = 1;
 if (!is_int($inputNumber)) {
     throw new InvalidArgumentException('main function only accepts integers. Input was: '.$inputNumber);
 }
-echo $task->main($inputNumber);
+echo $task_1->main($inputNumber);
---
+++ src/Task1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Prushak\Innowise;
+
+class Task1
+{
+    public function main(int $inputNumber)
+    {
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 20' : 'Equal or less than 10'));
+    }
+}
