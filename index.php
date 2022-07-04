62e4ab2cb1c7d7d5d1e103961b38fb0c09c11e46
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 03:33:38 +0300

    task-3

--- index.php
+++ index.php
@@ -3,8 +3,4 @@
 include_once 'src/Task3.php';
 $task_3 = new ('Prushak\Innowise\Task3');
 
-$inputNumber = 100;
-if (!is_int($inputNumber)) {
-    throw new InvalidArgumentException('main function only accepts integers. Input was: '.$inputNumber);
-}
-echo $task_3->main($inputNumber);
+echo $task_3->main(1997);
