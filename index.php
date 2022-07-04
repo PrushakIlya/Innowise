14f1a6518983a9e2808984cebc2dda6a54d36313
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 03:50:22 +0300

    task-7

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-include_once 'src/Task7.php';
+include_once 'Task7.php';
 $task_7 = new ('Prushak\Innowise\Task7');
 
-
-var_dump($task_7->main([1,2,4], 6));
+var_dump($task_7->main([1,2,4,4,5], 3));
