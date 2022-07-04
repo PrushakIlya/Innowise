2a3e1f0e9c37500e158600ea02ed8adfb08a092c
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 12:49:23 +0300

    task-8

--- Task8.php
+++ Task8.php
@@ -6,17 +6,18 @@ class Task8
 {
     public function main(string $json)
     {
-        $result ='';
+        $result = '';
         preg_match_all('~"[a-zA-Z\s]+"(:\s?{)?~', $json, $mathes);
 
-        for ($i=0;$i<count($mathes[0])-1;) {
+        for ($i = 0;$i < count($mathes[0]) - 1;) {
             if (str_contains($mathes[0][$i], '{')) {
                 $i++;
             } else {
-                $result.= $mathes[0][$i].':'.$mathes[0][$i+1].'<br>';
-                $i+=2;
+                $result .= $mathes[0][$i].':'.$mathes[0][$i + 1].'<br>';
+                $i += 2;
             }
         }
+
         return $result;
     }
 }
