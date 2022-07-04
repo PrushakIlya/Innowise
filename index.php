c4a0c2293ed58ac0db12ec764a073308b5d50fef
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 03:31:47 +0300

    task-2

--- index.php
+++ index.php
@@ -3,9 +3,4 @@
 include_once 'src/Task2.php';
 $task_2 = new ('Prushak\Innowise\Task2');
 
-$date = 1;
-
-if (!is_string($date)) {
-    throw new InvalidArgumentException('main function only accepts strings and correct format. Input was: '.$date);
-}
-echo $task_2->main($date);
+echo $task_2->main('24.01.1997');
--- src/Task2.php
+++ src/Task2.php
@@ -19,7 +19,7 @@ class Task2
     }
     private function check(string $date)
     {
-        $match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|0?[1-9]{1}).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $date);
+        $match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|(0?[1-9]{1})).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $date);
         if ($match===0) {
             throw new InvalidArgumentException('main function only accepts correct format. Input was: '.$date);
         }
