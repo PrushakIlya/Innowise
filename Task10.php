e1d52f9835dfc823e622026a4235f92b7bf5c956
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 12:50:07 +0300

    task-10

--- Task10.php
+++ Task10.php
@@ -4,17 +4,18 @@ namespace Prushak\Innowise;
 
 class Task10
 {
-    public function main(int $input):array
+    public function main(int $input): array
     {
-        $result = array($input);
-        while ($input!==1) {
-            if ($input%2) {
-                $input = 3*$input+1;
+        $result = [$input];
+        while ($input !== 1) {
+            if ($input % 2) {
+                $input = 3 * $input + 1;
             } else {
-                $input = $input/2;
+                $input = $input / 2;
             }
             array_push($result, $input);
         }
+
         return $result;
     }
 }
