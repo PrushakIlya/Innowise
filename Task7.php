8f039da3a9eb3a015c844227ec457fbbd40a5688
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-04 12:48:59 +0300

    task-7

--- Task7.php
+++ Task7.php
@@ -9,16 +9,17 @@ class Task7
     public function main(array $arr, int $position)
     {
         $this->check($arr, $position);
-        foreach ($arr as $key=>$item) {
+        foreach ($arr as $key => $item) {
             if ($key === $position) {
                 unset($arr[$key]);
             }
         }
+
         return $arr;
     }
     public function check(array $arr, int $position)
     {
-        if ($position>=count($arr)) {
+        if ($position >= count($arr)) {
             throw new InvalidArgumentException('That position is not correct');
         }
     }
--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 include_once 'Task7.php';
 $task_7 = new ('Prushak\Innowise\Task7');
 
-var_dump($task_7->main([1,2,4,4,5], 3));
+var_dump($task_7->main([1, 2, 4, 4, 5], 3));
