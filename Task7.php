9277d9c2f2c1560a42110e76b8e89800f84eda31
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 20:39:06 +0300

    task-7

--- Task7.php
+++ Task7.php
@@ -9,18 +9,22 @@ class Task7
     public function main(array $arr, int $position): array
     {
         $this->check($arr, $position);
+        $result = [];
         foreach ($arr as $key => $item) {
             if ($key === $position) {
                 unset($arr[$key]);
             }
         }
+        foreach ($arr as $item) {
+            array_push($result, $item);
+        }
 
-        return $arr;
+        return $result;
     }
     public function check(array $arr, int $position)
     {
-        if ($position >= count($arr)) {
-            throw new InvalidArgumentException('That position is not correct');
+        if ($position >= count($arr) || $position < 0 || empty($arr)) {
+            throw new InvalidArgumentException('The position or array is not correct');
         }
     }
 }
--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 include_once 'Task7.php';
 $task_7 = new ('src\Task7');
 
-var_dump($task_7->main([1, 2, 3, 4, 5], 3));
+var_dump($task_7->main([1], 0));
