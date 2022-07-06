0f2207d57c26ade7b247d1f68b9073f063191d97
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 15:35:08 +0300

    task-1

--- Task1.php
+++ Task1.php
@@ -4,7 +4,7 @@ namespace Prushak\Innowise;
 
 class Task1
 {
-    public function main(int $inputNumber)
+    public function main(int $inputNumber): int
     {
         return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equal or less than 10'));
     }
