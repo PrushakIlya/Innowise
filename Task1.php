f69f85ff6f04b81ad8eba55beded591782b35851
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 19:20:53 +0300

    task-1

--- Task1.php
+++ Task1.php
@@ -1,18 +1,11 @@
 <?php
 
-namespace Prushak\Innowise;
+namespace src;
 
 class Task1
 {
-    private int $inputNumber;
-
-    public function __construct(int $inputNumber)
+    public function main(int $inputNumber): string
     {
-        $this->inputNumber = $inputNumber;
-    }
-
-    public function main(): string
-    {
-        return $this->inputNumber > 30 ? 'More than 30' : ($this->inputNumber > 20 ? 'More than 20' : ($this->inputNumber > 10 ? 'More than 10' : 'Equal or less than 10'));
+        return $inputNumber > 30 ? 'More than 30' : ($inputNumber > 20 ? 'More than 20' : ($inputNumber > 10 ? 'More than 10' : 'Equal or less than 10'));
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 include_once 'Task1.php';
-$input = 11;
+$input = 31;
 if (!is_int($input)) {
     throw new InvalidArgumentException('main function only accepts integer. Input was: '.$input);
 }
-$task_1 = new ('Prushak\Innowise\Task1')($input);
+$task_1 = new ('src\Task1');
 
-echo $task_1->main();
+echo $task_1->main($input);
