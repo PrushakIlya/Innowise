431a64376a017c3fced7563f0b4e0733bcebd440
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 19:32:42 +0300

    task-12

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once 'Task12.php';
-$task_12 = new ('Prushak\Innowise\Task12')(12, 6);
+$task_12 = new ('src\Task12')(12, 6);
 echo $task_12->add()->divideBy(2)->main();
 // echo $task_12->multiply()->main();
 // echo $task_12->subtract()->main();
