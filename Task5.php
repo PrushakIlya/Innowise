16981781a57cc90fd5a8342760144e057dc3ab1e
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 15:17:19 +0300

    ефыл-5

--- Task5.php
+++ Task5.php
@@ -4,19 +4,61 @@ namespace Prushak\Innowise;
 
 class Task5
 {
-    public function main(int $num): float
-    {
-        $s = sqrt(5);
-        $p = ($s + 1) / 2;
+    // public function main(int $num): float
+    // {
+    //     $s = sqrt(5);
+    //     $p = ($s + 1) / 2;
 
-        return (pow($p, $num) / $s + 0.5);
+    //     return (pow($p, $num) / $s + 0.5);
+    // }
+    public function main(int $n): int
+    {
+        $num_1 = 0;
+        $left = 0;
+        $str = '';
+        $num_2 = 1;
+        while (strlen((string)$num_2) < $n) {
+            $a = 0;
+            $result = [];
+            $two = $num_2;
+            $str_2 = str_split($num_2);
+            $str_1 = str_split($num_1);
+            $temp = str_split($num_2);
+            for ($i = 0;$i < count($temp);$i++) {
+                $temp_2 = array_pop($str_2);
+                if ($left === 1) {
+                    (int)$temp_2 += $left;
+                    $left = 0;
+                }
+                if (empty($str_1)) {
+                    $temp_1 = 0;
+                } else {
+                    (int)$temp_1 = array_pop($str_1);
+                }
+                $a = (int)$temp_1 + (int)$temp_2;
+
+                if ($a > 9) {
+                    $left = 1;
+                    $a = str_split($a);
+                    $a = array_pop($a);
+                };
+                $str .= $a;
+            }
+            $num_2 = (int)strrev($str);
+            $num_1 = $two;
+        };
+
+        return $num_2;
     }
     // public function main(int $n): int
     // {
     //     $num_1 = 0;
     //     $num_2 = 1;
+    //     $result = 0;
     //     while (strlen((string)$num_2) < $n) {
     //         $temp = $num_2;
+    //         for ($i = 0;$i < strlen((string)$num_2);$i++) {
+    //         }
     //         $num_2 = $num_1 + $num_2;
     //         $num_1 = $temp;
     //     };
--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 include_once 'Task5.php';
 $task_5 = new ('Prushak\Innowise\Task5');
 
-echo $task_5->main(500);
+echo $task_5->main(3);
