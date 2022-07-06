6aafa362dd6876cede1d332cbdc476c6c37deb4b
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 16:11:16 +0300

    task-1

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 
 include_once 'Task1.php';
 $task_1 = new ('Prushak\Innowise\Task1');
-
+$input = 5;
+if (!is_int($input)) {
+    throw new InvalidArgumentException('main function only accepts integer. Input was: '.$input);
+}
 echo $task_1->main(10);
