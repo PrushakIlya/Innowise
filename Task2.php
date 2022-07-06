730444dce1e2eafcc2a8848f10282a6e0fa7be6e
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-06 16:28:22 +0300

    task-2

--- Task2.php
+++ Task2.php
@@ -7,10 +7,16 @@ use InvalidArgumentException;
 
 class Task2
 {
-    public function main(string $date): string
+    private string $date;
+
+    public function __construct(string $date)
     {
-        $this->check($date);
-        $date = explode('.', $date);
+        $this->date = $date;
+    }
+    public function main(): string
+    {
+        $this->check();
+        $date = explode('.', $this->date);
         $time = date('d.m.y');
         $time = explode('.', $time);
         $year = 2023;
@@ -18,11 +24,11 @@ class Task2
 
         return date_diff(new DateTime(), new DateTime($year.'-'.$date[1].'-'.$date[0]))->days;
     }
-    private function check(string $date)
+    private function check()
     {
-        $match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|(0?[1-9]{1})).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $date);
+        $match = preg_match('~^(((0|1|2)?[0-9]{1})|(30|31)|([1-9]{1})).((10|11|12)|(0?[1-9]{1})).((1900|19[0-9]{2})|(200[0-9]{1}|201[0-9]{1}|202[0-3]{1}))$~', $this->date);
         if ($match === 0) {
-            throw new InvalidArgumentException('main function only accepts correct format. Input was: '.$date);
+            throw new InvalidArgumentException('main function only accepts correct format. Input was: '.$this->date);
         }
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 include_once 'Task2.php';
-$task_2 = new ('Prushak\Innowise\Task2');
+$task_2 = new ('Prushak\Innowise\Task2')('24.01.1997');
 
-echo $task_2->main('24.01.1997');
+echo $task_2->main();
