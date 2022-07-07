d39c6811fb0d4c8c302605f9f32d7a8301e3838d
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-07 11:58:13 +0300

    task-3

--- Task3.php
+++ Task3.php
@@ -10,21 +10,15 @@ class Task3
     {
         $this->check($input);
         while (strlen((string)$input) !== 1) {
-            $sum = 0;
             $arr = str_split($input);
-            foreach ($arr as $item) {
-                $sum += (int)$item;
-            }
-            $input = $sum;
-            // $arr = str_split($input); //second version
-            // $input = array_sum($arr);
+            $input = array_sum($arr);
         }
 
         return $input;
     }
     private function check(int $input)
     {
-        if ($input < 0) {
+        if ($input < 10) {
             throw new InvalidArgumentException('main function only accepts positive number. Input was: '.$input);
         }
     }
