9113ba63def838776358c6f18a20ac32ab4440ef
Ilya <89218019+PrushakIlya@users.noreply.github.com>
2022-07-07 15:30:36 +0300

    test-9

--- Task9.php
+++ Task9.php
@@ -11,22 +11,19 @@ class Task9
         $this->check($arr, $number);
         $result = [];
         for ($i = 0;$i < count($arr) - 2;$i++) {
-            $temp = [];
             $sum = $arr[$i] + $arr[$i + 1] + $arr[$i + 2];
-            $sum === $number && array_push($temp, $arr[$i].' + '.$arr[$i + 1].' + '.$arr[$i + 2].' = '.$sum) && array_push($result, $temp);
+            $sum === $number && array_push($result, $arr[$i].' + '.$arr[$i + 1].' + '.$arr[$i + 2].' = '.$sum);
         }
 
         return $result;
     }
     private function check(array $arr, int $number)
     {
-        if (empty($arr) || count($arr) !== 3 || $number < 1) {
+        if (empty($arr) || count($arr) < 3 || $number < 1) {
             throw new InvalidArgumentException('Incorrect');
         }
         foreach ($arr as $item) {
-            if ($item < 0) {
-                throw new InvalidArgumentException('Incorrect');
-            }
+            $item < 0 && throw new InvalidArgumentException('Incorrect');
         }
     }
 }
--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 include_once 'Task9.php';
 $task = new ('src\Task9');
 
-var_dump($task->main([2, -1, 1], 10));
+var_dump($task->main([2, 7, 7, 1, 8, 2, 7, 8, 7], 16));
